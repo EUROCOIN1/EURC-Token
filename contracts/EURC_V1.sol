ee7ba8e49318aa334502196c98c8cecfad7057f1
EUROCOIN1 <multitradingnj@gmail.com>
2025-07-11 00:26:00 +0530

    Update EURC_V1.sol

--- contracts/EURC_V1.sol
+++ contracts/EURC_V1.sol
@@ -1,1 +1,35 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract EURC_V1 is Initializable {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function initialize() public initializer {
+        name = "Euro Coin";
+        symbol = "EURC";
+        decimals = 6;
+    }
+
+    function mint(address to, uint256 amount) public {
+        balanceOf[to] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), to, amount);
+    }
+
+    function transfer(address to, uint256 amount) public returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Not enough");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+}
